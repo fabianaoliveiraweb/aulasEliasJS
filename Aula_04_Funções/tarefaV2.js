d5e9311a5439dd84dd127e2a7d519bcebc880ee8
fabiana.silva <fabiana.silva@ideris.com.br>
2024-07-18 19:07:07 -0300

    tarefas aula 4 funções

--- Aula_04_Funções/tarefaV2.js
+++ Aula_04_Funções/tarefaV2.js
@@ -3,21 +3,21 @@ Na compra em um minimercado, entrou com 100 reais, ele tem q comprar 1 saco de a
 1 saco de feijão, 1 saco de sal. Ver se o dinheiro deu, dar o troco, senão, troco é negativo, retirar produtos do carrinho. 
 Criar variáveis pra cada produto e para o dinheiro na carteira. 
 */
- function mercado() {    
+function mercado() {    
     dinheiro = 100;
     arroz = 28.00;
     feijao = 10.00;
     acucar = 5.00;
-    sal = 3.00;
+    sal = 30.00;
     somaCompra = (arroz + feijao + acucar + sal);
     troco = (dinheiro - somaCompra);
 
+    //UTILIZANDO TO.FIXED - DUAS CASAS DECIMAIS 
     if(somaCompra > dinheiro){
         return console.log("Você não possui dinheiro para a compra. Favor retirar itens do carrinho. Valor total: R$", somaCompra.toFixed(2));
     }else {
-        + console.log("Você possui dinheiro para a compra. O valor total é de R$" + somaCompra + " e o troco de R$", troco) +
-            console.log("Você está comprando: " + "\n" + "- 1 saco de arroz" + "\n" + "- 1 saco de feijão" + "\n" + 
-                "- 1 saco de açúcar"  + "\n" + "- 1 saco de sal");  
+        return console.log(`- Arroz: R$ ${arroz.toFixed(2)}`); //template string      
     }
  }
+
  mercado();
