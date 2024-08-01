243c3de571e1b5cb5064eb18255376b6e81faf59
Fabiana <fabiana.silva@ideris.com.br>
2024-08-01 19:03:54 -0300

    Aula DOM

---
+++ Aula_06_DOM/script.js
@@ -0,0 +1,15 @@
+function mostrarMensagem() {
+
+    let mensagem = "Seja bem vindo ";
+
+    const resultadoDiv = document.getElementById("mostrarMensagem");
+
+    resultadoDiv.textContent = mensagem;
+
+    let mensagem2 = "Hello World";
+
+    const resultadoDiv2 = document.getElementById("mostrarMensagem2");//pega
+
+    resultadoDiv2.textContent = mensagem2;//mostra
+
+}
